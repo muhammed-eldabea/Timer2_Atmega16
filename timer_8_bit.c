78c63c0ac6f262bafdf5cfa1eb6540f6c2c297fc
muhammed eldabea <43865045+muhammed-eldabea@users.noreply.github.com>
2020-06-09 11:05:19 +0200

    timer 2 implementation

---
+++ timer_8_bit.c
@@ -0,0 +1,200 @@
+/******************************************************************************
+ *[Author]     	: Muhammed eldabea Hashem
+ *
+ *[filename]   	: timer_8_bit.c
+ *
+ *[Date]       	: 9 June 2020
+ *
+ *[Description] : contain all function implementation that used in 8 bit timer
+ *					in  ATmega 16
+ *
+ ******************************************************************************/
+
+
+
+/*=============================================================================
+ * 									Include
+ *=============================================================================*/
+
+#include "timer_8_bit.h"
+
+/*=============================================================================
+ * 								Global Variable
+ *=============================================================================*/
+
+/*a global pointer to function used in timer2 ISR callBack*/
+volatile void (*ptrFunc)(Void) = Null ;
+
+
+/*=============================================================================
+ * 							Function Definition
+ *=============================================================================*/
+
+ISR(TIMER2_COMP_vect)
+{
+	/*=========================================================================
+		[Function Name]  : ISR(TIMER2_COMP_vect)
+		[Description]    : Interrupt service routine for compare match
+	==========================================================================*/
+
+
+	/*check if the pointer hold no address */
+	if(ptrFunc != Null) {
+		(*ptrFunc)() ;
+	}
+}
+
+
+ISR (TIMER2_OVF_vect)
+{
+	/*=========================================================================
+		[Function Name]  : ISR(TIMER2_OVF_vect)
+		[Description]    : Interrupt service routine for overFlow
+	==========================================================================*/
+
+
+	/*check if the pointer hold no address */
+	if(ptrFunc != Null) {
+		(*ptrFunc)() ;
+	}
+
+}
+
+
+
+
+void Timer2_init(Timer2_Configuration *ptrStruct )
+{
+
+	/*==========================================================================
+	[FUNCTION NAME] : Timer2_init
+
+	[DESRIPTION]    : initialize the timer2 module be a given configuration
+					  send in a structure
+
+	[ARGUMENT(S)]  :
+	   [IN]  : pointer to Timer2_Configuration Structure
+
+	   [Return] : void
+
+
+	============================================================================*/
+
+	if (ptrStruct->Async_enable == 1 ){
+
+		ASSR |= (1<<3) ; /*set Asynchronous Timer/Counter2 */
+		/* To switch to asynchronous operation: Wait for TCN2UB, OCR2UB, and
+	   TCR2UB */
+		while((BIT_IS_CLEAR(ASSR,2)) &&
+				(BIT_IS_CLEAR(ASSR,1)) &&
+				(BIT_IS_CLEAR(ASSR,0)) ) {}
+
+
+	}
+
+	TCCR2 = (TCCR2 & 0xfc) | (ptrStruct->Frequency_prescaler  & 0x03) ; /*CLK*/
+	TCCR2 = (TCCR2 & 0xf8) | ((ptrStruct->Wave_generation_mode & 0x01)<<3) ;/*WGM21*/
+	TCCR2 = (TCCR2 & 0x4f) | ((ptrStruct->Wave_generation_mode & 0x02)<<5) ;/*WGM22*/
+	TCCR2 = (TCCR2 & 0x3f) | ((ptrStruct->Compare_output_mode & 0x03)<<4) ; /*COM*/
+
+	if(ptrStruct->Wave_generation_mode != PWM_PhaseCoorect_mode)
+	{
+		TCCR2 |= (1<<7) ; /*enable FOC2*/
+	}
+
+
+#if Interrupt_enable ==1
+	switch (ptrStruct->Wave_generation_mode)
+	{
+	case Normal_mode :
+		TIMSK |= (1<<6) ; /*Timer/Counter2 Overflow Interrupt Enable*/
+		break ;
+	case CTC_mode :
+		TIMSK |= (1<<7) ; /*Timer/Counter2 CTC Interrupt Enable*/
+		break ;
+
+
+	}
+
+#endif
+
+
+}
+
+
+
+
+void Timer2_OVF_Polling()
+{
+	/*=========================================================================
+		[Function Name]   : Timer2_OVF_Polling
+		[Description]     : polling to the overflow flag
+	 ==========================================================================*/
+
+
+	while(BIT_IS_CLEAR(TIFR,6)){}
+	TIFR |= (1<<6) ; /*clear the flag */
+}
+
+void Timer2_CTC_Polling()
+{
+	/*=========================================================================
+		[Function Name]   : Timer2_CTC_Polling
+		[Description]     : polling to the CTC flag
+     ==========================================================================*/
+
+	while(BIT_IS_CLEAR(TIFR,7)){}
+	TIFR |= (1<<7) ; /*clear the flag */
+}
+
+
+void Timer2Clear ()
+{
+	/*=========================================================================
+			[Function Name]   : Timer2clear
+			[Description]     : clear timer2 register
+     ==========================================================================*/
+
+	TCNT2 = 0 ;
+	OCR2  = 0 ;
+
+}
+
+
+
+void Timer2_SetCompareValue(uint8 value)
+{
+	/*=========================================================================
+			[Function Name]   : Timer2_SetCompareValue
+			[Description]     : set the compare value
+	 ==========================================================================*/
+
+	OCR2 = value ;
+}
+
+void Timer2_Setinitail_Value(uint8 value)
+{
+	/*=========================================================================
+			[Function Name]   : Timer2_Setinitail_Value
+			[Description]     : set the initial  value
+	 ==========================================================================*/
+
+	TCNT2 = value ;
+}
+
+
+
+void Timer2_callBach(void(*ptr)(void))
+{
+
+	/*=========================================================================
+			[Function Name]   : Timer2_callBach
+			[Description]     : function will be called in ISR
+			[Arguments]       : a pointer to void function
+     ==========================================================================*/
+
+
+	ptrFunc =(volatile)ptr ;
+}
+
+
---
+++ timer_8_bit.h
@@ -0,0 +1,108 @@
+
+/*--------------------------------------------------------------------------------------------------------------
+
+ &[FILE NAME]    : timer_8_bit.h
+
+ &[AUTHOR(S)]    : Muhammed Eldabea Hashem
+
+ &[DATE CREATED] : 20 April 2020
+
+ &[DESCRIPTION]  :  definitions and configuration used foe Timer2 module in Atmega16
+
+------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+#ifndef TIMER_8_BIT_H_
+#define TIMER_8_BIT_H_
+
+
+
+
+
+/*---------------------------------------> INCLUDES <---------------------------------------*/
+
+#include  <avr/io.h>
+#include <avr/delay.h>
+
+
+/*-------------------------------> Definitions and configuration <-------------------------*/
+
+typedef unsigned char uint8   ;
+
+#define Interrupt_enable    0
+#define Null                (void*)0
+
+typedef enum {
+	/*wave generation mode of the timer
+	 * the mode by which the timer will work */
+
+	Normal_mode , PWM_PhaseCoorect_mode , CTC_mode , Fast_PWM_mode
+}Wave_Generation_mode;
+
+typedef enum {
+	/* the action that will be taken when output compare action
+	   happen when non PWM is selected  */
+	Normal_Port_OC2_Diconnected_NON_PWM , Toggle_OC2_on_compare_match_NON_PWM,
+	Clear_OC2_on_Compare_match_Non_PWM,Set_OC2_match_Non_PWM
+}Compare_output_mode_non_PWM_mode;
+
+typedef enum {
+	/*the action that will be taken when output compare action
+	   happen when Fast PWM is selected  */
+	Normal_Port_OC2_Diconnected_FastPWM , Toggle_OC2_on_compare_match_FastPWM,
+	Clear_OC2_on_Compare_match_FastPWM,Set_OC2_match_FastPWM
+}Compare_output_mode_Fast_PWM_mode;
+
+typedef enum {
+	/*select the frequency prescaler */
+	NO_Clock_source,FrqBY_0_NoPrescaler,FrqBY_8_Prescaler,FrqBY_32_Prescaler,
+	FrqBY_64_Prescaler,FrqBY_128_Prescaler,FrqBY_256_Prescaler,
+	FrqBY_1025_Prescaler
+
+
+}Clock_presclaer;
+
+typedef enum {
+	/*select the clock source by which the timer will work */
+	synch_clockSource,Asynch_clockSource
+
+}Clock_Source;
+
+
+
+/*--------------------------> Structure and Unions <--------------------------*/
+typedef struct
+{
+
+	/*=========================================================================
+	[Structure Name]  : Timer2_Configuration
+	[Description]     : Select the configuration of the timer2
+	 ==========================================================================*/
+
+	uint8 Wave_generation_mode:2 ; /*select the mode by which the timer will work */
+	uint8 Compare_output_mode :2 ; /* the operation of the oc2 when output compare */
+	uint8 Frequency_prescaler :3 ; /*control the frequency that will be used */
+	uint8 Async_enable        :1 ; /*Asynchronous Timer/Counter2*/
+
+
+}Timer2_Configuration;
+
+/*-------------------------------> Macros <------------------------------------*/
+
+
+#define BIT_IS_CLEAR(REG,BIT) ( !(REG & (1<<BIT)) )
+
+/*-------------------------> Function Declarations <----------------------------*/
+
+
+
+
+
+
+
+
+
+
+#endif /* TIMER_8_BIT_H_ */
